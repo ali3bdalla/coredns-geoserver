d1744d948e965e3b4810436c09ce13c4e6f393b1
ali abdalla <ali.dev.sd@gmail.com>
2022-11-26 23:34:25 +0100

    init commit

---
+++ cdn_test.go
@@ -0,0 +1,37 @@
+package cdn
+
+import (
+	"bytes"
+	"context"
+	golog "log"
+	"strings"
+	"testing"
+
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/test"
+
+	"github.com/miekg/dns"
+)
+
+func TestCDN(t *testing.T) {
+	// Create a new Example Plugin. Use the test.ErrorHandler as the next plugin.
+    x := CDN{Next: test.ErrorHandler()}
+
+	// Setup a new output buffer that is *not* standard output, so we can check if
+	// example is really being printed.
+	b := &bytes.Buffer{}
+	golog.SetOutput(b)
+
+	ctx := context.TODO()
+	r := new(dns.Msg)
+	r.SetQuestion("cdn.org.", dns.TypeA)
+	// Create a new Recorder that captures the result, this isn't actually used in this test
+	// as it just serves as something that implements the dns.ResponseWriter interface.
+	rec := dnstest.NewRecorder(&test.ResponseWriter{})
+
+	// Call our plugin directly, and check the result.
+	x.ServeDNS(ctx, rec, r)
+	if a := b.String(); !strings.Contains(a, "[INFO] plugin/cdn: cdn") {
+		t.Errorf("Failed to print '%s', got %s", "[INFO] plugin/cdn: cdn", a)
+	}
+}
