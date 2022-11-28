facf72cedc448f5a10be5aacf2d57536cc3a7e86
ali abdalla <ali.dev.sd@gmail.com>
2022-11-28 16:23:03 +0100

    add wip

--- cdn.go
+++ cdn.go
@@ -1,22 +1,24 @@
-// Package example is a CoreDNS plugin that prints "example" to stdout on every packet received.
+// Package cdn Package example is a CoreDNS plugin that prints "example" to stdout on every packet received.
 //
 // It serves as an example CoreDNS plugin with numerous code comments.
 package cdn
 
 import (
 	"context"
+	"fmt"
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/metadata"
 	"github.com/coredns/coredns/plugin/metrics"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
-    "github.com/coredns/coredns/plugin/metadata"
 	"github.com/miekg/dns"
+	"strconv"
 )
 
 // Define log to be a logger with the plugin name in it. This way we can just use log.Info and
 // friends to log.
 var log = clog.NewWithPlugin("cdn")
 
-// Example is an example plugin to show how to write a plugin.
+// CDN Example is an example plugin to show how to write a plugin.
 type CDN struct {
 	Next plugin.Handler
 }
@@ -24,20 +26,26 @@ type CDN struct {
 // ServeDNS implements the plugin.Handler interface. This method gets called when example is used
 // in a Server.
 func (e CDN) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	if getContinentCode := metadata.ValueFunc(ctx, "geoip/continent/code"); getContinentCode != nil {
+		if name, err := strconv.ParseComplex(getContinentCode(), 64); err == nil {
+			fmt.Println(name)
+		}
+	}
+	//
 	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
 	// a slightly more complex example as to make this more interesting.
 	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
 	// answer comes back, it will print "example".
 
 	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
-    log.Debug("Received response",metadata.ValueFunc(ctx, "geoip/timezone"))
+	log.Debug("Received response", metadata.ValueFunc(ctx, "geoip/timezone"))
 
-//
-//    if getLongitude := metadata.ValueFunc(ctx, "geoip/longitude"); getLongitude != nil {
-//        if longitude, err := strconv.ParseFloat(getLongitude(), 64); err == nil {
-//            // Do something useful with longitude.
-//        }
-//    }
+	//
+	//    if getLongitude := metadata.ValueFunc(ctx, "geoip/longitude"); getLongitude != nil {
+	//        if longitude, err := strconv.ParseFloat(getLongitude(), 64); err == nil {
+	//            // Do something useful with longitude.
+	//        }
+	//    }
 	// Wrap.
 	pw := NewResponsePrinter(w)
 
